a38dc9706f5c88c6867ef3483cc688b9f4b7b7c0
ethan-shenker <eshenker10@stuy.edu>
2021-04-29 00:57:25 -0400

    try/catch blocks

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, redirect, url_for
 from uuid import uuid4
 from helpers import a_clean, get_greeting, tup_clean, a_remove, convert
 from time import localtime, strftime
@@ -20,7 +20,9 @@ dir += "/"
 ## To-do list functionality (all)
 ## error handling for if a username / password already exists
 
-
+'''
+fail safe!
+'''
 def random_error():
     return render_template("error.html", message="Something went wrong.", route="/")
 
@@ -30,12 +32,10 @@ root landing page
 '''
 @app.route("/", methods=["POST", "GET"])
 def root():
-    # try:
-        if session.get("username"):
-            return user_page()
-        return render_template("login.html")
-    # except:
-        # return random_error()
+    if session.get("username"):
+        return user_page()
+    return render_template("login.html")
+            
 
 '''
 logout function
@@ -65,67 +65,69 @@ all content contained on main user page within this function
 '''
 @app.route("/user_page")
 def user_page():
-    # ADVICE SLIP API
-    adv_data = urllib.request.urlopen("https://api.adviceslip.com/advice")
-    adv_readable = adv_data.read()
-    adv_d = json.loads(adv_readable)
-    slip = adv_d["slip"]
-    advice = slip["advice"]
+    try:
+        # ADVICE SLIP API
+        adv_data = urllib.request.urlopen("https://api.adviceslip.com/advice")
+        adv_readable = adv_data.read()
+        adv_d = json.loads(adv_readable)
+        slip = adv_d["slip"]
+        advice = slip["advice"]
 
-    # PUBLIC HOLIDAY API
-    API_KEY0 = open("keys/key_api0.txt", "r").read()
-    curr_time = strftime("%m:%d:%y", localtime())
-    month = curr_time[:2]
-    day = curr_time[3:5]
-    year = "20" + curr_time[6:]
+        # PUBLIC HOLIDAY API
+        API_KEY0 = open("keys/key_api0.txt", "r").read()
+        curr_time = strftime("%m:%d:%y", localtime())
+        month = curr_time[:2]
+        day = curr_time[3:5]
+        year = "20" + curr_time[6:]
 
-    holi_data = urllib.request.urlopen("https://holidays.abstractapi.com/v1/?api_key=" + API_KEY0 + "&country=US&year=" + year + "&month=" + month + "&day=" + day)
-    holi_readable = holi_data.read()
-    holi_d = json.loads(holi_readable)
-    if (len(holi_d) >= 1):
-        days = holi_d[0]
-        holiday = days["name"]
-    else:
-        holiday = "No Holiday(s) Today"
+        holi_data = urllib.request.urlopen("https://holidays.abstractapi.com/v1/?api_key=" + API_KEY0 + "&country=US&year=" + year + "&month=" + month + "&day=" + day)
+        holi_readable = holi_data.read()
+        holi_d = json.loads(holi_readable)
+        if (len(holi_d) >= 1):
+            days = holi_d[0]
+            holiday = days["name"]
+        else:
+            holiday = "No Holiday(s) Today"
 
-    ## WEATHER API
-    API_KEY1 = open("keys/key_api1.txt", "r").read()
+        ## WEATHER API
+        API_KEY1 = open("keys/key_api1.txt", "r").read()
 
-    weather = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=" + API_KEY1).read()
-    weather = json.loads(weather.decode('utf-8'))
-    weather_dict = {}
-    weather_dict['main'] = weather['weather'][0]['main']
-    weather_dict['temp'] = int(9.0/5.0*(weather['main']['temp'] - 273) + 32)
+        weather = urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=" + API_KEY1).read()
+        weather = json.loads(weather.decode('utf-8'))
+        weather_dict = {}
+        weather_dict['main'] = weather['weather'][0]['main']
+        weather_dict['temp'] = int(9.0/5.0*(weather['main']['temp'] - 273) + 32)
 
 
-    # DOG PHOTO API
-    u = urllib.request.urlopen("https://dog.ceo/api/breeds/image/random")
-    response = u.read()
-    data = json.loads( response )
-    pic = data['message']
+        # DOG PHOTO API
+        u = urllib.request.urlopen("https://dog.ceo/api/breeds/image/random")
+        response = u.read()
+        data = json.loads( response )
+        pic = data['message']
 
-    # RETRIEVE USER NOTE
-    db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
-    c = db.cursor()
-    c.execute("SELECT content FROM user_note WHERE user_id=?", (str(session.get("user_id")),))
-    prev_content = tup_clean(c) # returns list of each element from cursor
+        # RETRIEVE USER NOTE
+        db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
+        c = db.cursor()
+        c.execute("SELECT content FROM user_note WHERE user_id=?", (str(session.get("user_id")),))
+        prev_content = tup_clean(c) # returns list of each element from cursor
 
-    if len(prev_content) > 0: # if the user already had a note saved from a previous session
-        note = a_remove(prev_content[0])
-    else:
-        note = "Write anything here, and click the Save button below to save your work for the future!"
+        if len(prev_content) > 0: # if the user already had a note saved from a previous session
+            note = a_remove(prev_content[0])
+        else:
+            note = "Write anything here, and click the Save button below to save your work for the future!"
 
-    c.execute("SELECT title, body, item_id FROM todo WHERE user_id=? ORDER BY date_time", (str(session.get("user_id")),))
-    todo_tuple = list(c)
-    num_items_already_in_list = len(todo_tuple)
+        c.execute("SELECT title, body, item_id FROM todo WHERE user_id=? ORDER BY date_time", (str(session.get("user_id")),))
+        todo_tuple = list(c)
+        num_items_already_in_list = len(todo_tuple)
 
-    if num_items_already_in_list > 0:
-        todo_list = convert(todo_tuple)
-    else:
-        todo_list = []
+        if num_items_already_in_list > 0:
+            todo_list = convert(todo_tuple)
+        else:
+            todo_list = []
 
-    return render_template("user_page.html", greeting=get_greeting(session.get("username")), adv=advice, holi=holiday, user_note=note, picture=pic, to_dos=todo_list, weather=weather_dict, route="/")
-
+        return render_template("user_page.html", greeting=get_greeting(session.get("username")), adv=advice, holi=holiday, user_note=note, picture=pic, to_dos=todo_list, weather=weather_dict, route="/")
+    except:
+        random_error()
 
 
 '''
@@ -133,20 +135,23 @@ login function
 '''
 @app.route("/login", methods=["POST"])
 def login():
-    # based on form, have form.request['elem'] tags here
-    username = request.form["username"]
-    password = request.form["password"]
+    try:
+        # based on form, have form.request['elem'] tags here
+        username = request.form["username"]
+        password = request.form["password"]
 
-    db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
-    c = db.cursor()
-    c.execute("SELECT password, user_id FROM users WHERE username=?", (username,))
-    accounts = list(c) #returns tuple
-    if len(accounts) != 1 or password != accounts[0][0]:
-        return auth_error(is_login_incorrect=True)
-    else:
-        session['username'] = username # add user and user_id to session for auth purposes
-        session['user_id'] = accounts[0][1]
-    return root()
+        db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
+        c = db.cursor()
+        c.execute("SELECT password, user_id FROM users WHERE username=?", (username,))
+        accounts = list(c) #returns tuple
+        if len(accounts) != 1 or password != accounts[0][0]:
+            return auth_error(is_login_incorrect=True)
+        else:
+            session['username'] = username # add user and user_id to session for auth purposes
+            session['user_id'] = accounts[0][1]
+        return redirect(url_for('root'))
+    except:
+        random_error()
 
 @app.route("/create_user", methods=['POST'])
 def create_user():
@@ -157,79 +162,92 @@ register function, registers new users
 '''
 @app.route("/register", methods=["POST"])
 def register():
-    username = request.form["username"]
-    password = request.form["password"]
+    try:
+        username = request.form["username"]
+        password = request.form["password"]
 
-    db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
-    c = db.cursor()
-    c.execute("SELECT username FROM users")
+        db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
+        c = db.cursor()
+        c.execute("SELECT username FROM users")
 
-    pre_existing_usernames = convert(list(c))
-    print(pre_existing_usernames)
+        pre_existing_usernames = convert(list(c))
+        print(pre_existing_usernames)
 
-    if [username] in pre_existing_usernames:
-        return auth_error(is_user_conflict=True)
-    else:
-        user_id = uuid4() # generate new uuid for user
-        c.execute("INSERT INTO users (user_id, username, password) VALUES (?, ?, ?)", (str(user_id), username, password,))
-        session['username'] = str(username)
-        session['user_id'] = user_id
-        db.commit()
-        return user_page()
-    return root()
-
+        if [username] in pre_existing_usernames:
+            return auth_error(is_user_conflict=True)
+        else:
+            user_id = uuid4() # generate new uuid for user
+            c.execute("INSERT INTO users (user_id, username, password) VALUES (?, ?, ?)", (str(user_id), username, password,))
+            session['username'] = str(username)
+            session['user_id'] = user_id
+            db.commit()
+            return user_page()
+        return root()
+    except:
+        random_error() 
 
 '''
 any time user wishes to save notes content, it will come from here
 '''
 @app.route("/notes_update", methods=["POST"])
 def update_note():
-    user_id = session.get("user_id")
-    new_content = a_clean(request.form["notes"])
+    try:
+        user_id = session.get("user_id")
+        new_content = a_clean(request.form["notes"])
 
-    db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
-    c = db.cursor()
+        db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
+        c = db.cursor()
 
-    c.execute("DELETE FROM user_note WHERE user_id=?", (str(user_id),)) # remove old note from db
-    c.execute("INSERT INTO user_note (user_id, content) VALUES (?, ?);", (str(user_id), new_content,)) # add new one!
-    db.commit()
+        c.execute("DELETE FROM user_note WHERE user_id=?", (str(user_id),)) # remove old note from db
+        c.execute("INSERT INTO user_note (user_id, content) VALUES (?, ?);", (str(user_id), new_content,)) # add new one!
+        db.commit()
 
-    return user_page()
+        return redirect(url_for('root'))
+    except:
+        random_error()
 
 @app.route("/clear_all", methods=["POST"])
 def clear_todo_list():
-    user_id = session.get("user_id")
-    db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
-    c = db.cursor()
-    c.execute("DELETE FROM todo WHERE user_id=?", (str(user_id),))
-    db.commit()
-    return root()
+    try:
+        user_id = session.get("user_id")
+        db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
+        c = db.cursor()
+        c.execute("DELETE FROM todo WHERE user_id=?", (str(user_id),))
+        db.commit()
+        return redirect(url_for('root'))
+    except:
+        random_error()
 
-# TODO : bug test and makes sure this works properly
+
 @app.route("/add_item", methods=["POST"])
 def add_item_todo():
-    user_id = session.get("user_id")
-    db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
-    c = db.cursor()
-    item_id = str(uuid4())
-    item_title = request.form['title']
-    item_body = request.form['description']
-    date_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
-    c.execute("INSERT INTO todo (user_id, item_id, title, body, date_time) VALUES (?, ?, ?, ?, ?)", (str(user_id), item_id, item_title, item_body, date_time,))
-    db.commit()
-    return root()
+    try:
+        user_id = session.get("user_id")
+        db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
+        c = db.cursor()
+        item_id = str(uuid4())
+        item_title = request.form['title']
+        item_body = request.form['description']
+        date_time = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        c.execute("INSERT INTO todo (user_id, item_id, title, body, date_time) VALUES (?, ?, ?, ?, ?)", (str(user_id), item_id, item_title, item_body, date_time,))
+        db.commit()
+        return redirect(url_for('root'))
+    except:
+        random_error()
 
 
 @app.route("/delete_item", methods=["POST"])
 def delete_item_todo():
-    user_id = session.get("user_id")
-    db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
-    c = db.cursor()
-    item_id = request.form["item_id"]
-    c.execute("DELETE FROM todo WHERE item_id=? AND user_id=?", (item_id, user_id,))
-    db.commit()
-    return root()
-
+    try:
+        user_id = session.get("user_id")
+        db = sqlite3.connect(dir + "lame.db") # dir + "blog.db") # connects to sqlite table
+        c = db.cursor()
+        item_id = request.form["item_id"]
+        c.execute("DELETE FROM todo WHERE item_id=? AND user_id=?", (item_id, user_id,))
+        db.commit()
+        return redirect(url_for('root'))
+    except:
+        random_error()
 
 # TODO: remember to delete!
 if __name__ == '__main__':
